59a1c6b4fe08d92bc83cbed13a0aebdf98919e9d
Aleksandra <s.tsikhmanovich@gmail.com>
2022-12-04 17:47:46 +0100

    feat: add interactivity

---
+++ pages/donate/script.js
@@ -0,0 +1,46 @@
+const menuOpen = document.querySelector('.menu-open');
+const burgerAll = '#burger';
+const burgerMenu = document.querySelector('.burger-menu');
+const burgerId = document.querySelector(burgerAll);
+const body = document.querySelector('body');
+
+let inputGroup = document.querySelectorAll('.radio_input');
+let customInput = document.querySelector('.custom_input');
+
+inputGroup.forEach(input => {
+    input.addEventListener('click', () => {
+        console.log(input.value)
+        customInput.value = input.value
+    })
+})
+
+customInput.addEventListener('change', () => {
+    let customValue = customInput.value;
+    inputGroup.forEach(input => {
+        input.removeAttribute('checked')
+        if( input.value === customValue) {
+            input.setAttribute('checked', true);
+        }
+    })
+})
+
+burgerMenu.addEventListener('click', () => {
+    burgerMenu.classList.toggle('active');
+    if( burgerMenu.classList.contains('active')) {
+        menuOpen.style.display = 'initial';
+        body.classList.add('hidden-body')
+    } else {
+        menuOpen.style.display = 'none'
+        body.classList.remove('hidden-body')
+
+    }
+})
+document.addEventListener('click', (e) => {
+    const isClosest = e.target.closest(burgerAll);
+    if (burgerMenu.classList.contains('active') && e.target !== burgerMenu && !isClosest) {
+        burgerMenu.classList.remove('active');
+        menuOpen.style.display = 'none'
+        body.classList.remove('hidden-body')
+    }
+})
+
